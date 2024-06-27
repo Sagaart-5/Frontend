5fea934bded1490d6ead990da12a1f3e177973ea
Elizaveta Ananeva-Ryashchenko <elizaveta.elenovna@yandex.ru>
2024-06-27 09:31:52 +0300

    feat: add appraisal section

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,25 +1,28 @@
-import { useState } from 'react'
 import Button from 'src/ui/Button/Button'
-import Popup from '../Popup/Popup'
 import styles from './Intro.module.scss'
 
 const Intro = () => {
-  const [isRegistrationOpen, setIsRegistrationOpen] = useState(false)
-
-  function handleOverlayClose() {
-    setIsRegistrationOpen(false)
-  }
-
-  function handleRegistrationOpen() {
-    setIsRegistrationOpen(true)
+  const scrollToSection = (id: string) => {
+    const element = document.getElementById(id)
+    element?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    })
   }
 
   return (
     <div className={styles.intro}>
       <div className={styles.overlay}>
         <div className={`${styles.content} ${styles.blur}`}>
-          <h1 className={styles.title}>Раскройте подлинную стоимость Арт объектов!</h1>
-          <Button title='Оценить' type='button' onClick={handleRegistrationOpen} />
+          <h1 className={styles.title}>
+            Раскройте подлинную стоимость Арт объектов!
+          </h1>
+          <Button
+            title='Оценить'
+            type='button'
+            onClick={() => scrollToSection('appraisal')}
+          />
         </div>
         <div></div>
         <div></div>
@@ -28,12 +31,6 @@ const Intro = () => {
         <div className={styles.blur}></div>
         <div></div>
       </div>
-      {isRegistrationOpen && (
-        <Popup
-          setIsRegistrationOpen={setIsRegistrationOpen}
-          handleOverlayClose={handleOverlayClose}
-        />
-      )}
     </div>
   )
 }
