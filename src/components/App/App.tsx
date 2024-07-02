c7b306c205ba9a573f8f85ca693dbe33398012fd
Elizaveta Ananeva-Ryashchenko <elizaveta.elenovna@yandex.ru>
2024-07-02 19:19:19 +0300

    feat: add api functional

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,8 @@
 import { Outlet } from 'react-router-dom'
 import Header from 'src/components/Header/Header'
 import styles from './App.module.scss'
-import { useEffect, useState } from 'react'
-import * as Api from 'src/services/utils'
 
 const App = () => {
-  const [arts, getArts] = useState([])
-  console.log('arts: ', arts)
-
-  useEffect(() => {
-    Api.getArts()
-      .then(data => {
-        getArts(data)
-        console.log('data: ', data)
-      })
-      .catch(error => {
-        console.error(error)
-      })
-  }, [])
-
   return (
     <div className={styles.app}>
       <Header />
