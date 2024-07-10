91adbf0aae9a8512838672d8f4e0ad126f44f9de
Elizaveta Ananeva-Ryashchenko <elizaveta.elenovna@yandex.ru>
2024-07-10 23:03:01 +0300

    feat: add mock data

---
+++ src/utils/mock/imports.ts
@@ -0,0 +1,148 @@
+// Events
+import Event1 from 'src/assets/images/mock/events/event1.jpg'
+import Event2 from 'src/assets/images/mock/events/event2.jpg'
+import Event3 from 'src/assets/images/mock/events/event3.jpg'
+import Event4 from 'src/assets/images/mock/events/event4.jpg'
+import Event5 from 'src/assets/images/mock/events/event5.jpg'
+import Event6 from 'src/assets/images/mock/events/event6.jpg'
+import Event7 from 'src/assets/images/mock/events/event7.jpg'
+import Event8 from 'src/assets/images/mock/events/event8.jpg'
+
+// Abstraction
+import Crowd from 'src/assets/images/mock/arts/abstraction/crowd.jpg'
+import Eye from 'src/assets/images/mock/arts/abstraction/eye.jpg'
+import Jellyfish from 'src/assets/images/mock/arts/abstraction/jellyfish.jpg'
+import Music from 'src/assets/images/mock/arts/abstraction/music.jpg'
+import Shine from 'src/assets/images/mock/arts/abstraction/shine.jpg'
+import Smile from 'src/assets/images/mock/arts/abstraction/smile.jpg'
+import Space from 'src/assets/images/mock/arts/abstraction/space.jpg'
+import Uturn from 'src/assets/images/mock/arts/abstraction/u-turn.jpg'
+
+// Minimalism
+import EasyConversation from 'src/assets/images/mock/arts/minimalism/easy_conversation.jpg'
+import Fatigue from 'src/assets/images/mock/arts/minimalism/fatigue.jpg'
+import MountainsAndRiver from 'src/assets/images/mock/arts/minimalism/mountains_and_river.jpg'
+import Road from 'src/assets/images/mock/arts/minimalism/road.jpg'
+
+// Expressionism
+import Dance from 'src/assets/images/mock/arts/expressionism/dance.jpg'
+import Farmer from 'src/assets/images/mock/arts/expressionism/farmer.jpg'
+import LastSupper from 'src/assets/images/mock/arts/expressionism/last_supper.jpg'
+import Palette from 'src/assets/images/mock/arts/expressionism/palette.jpg'
+import Robbers from 'src/assets/images/mock/arts/expressionism/robbers.jpg'
+import Scientist from 'src/assets/images/mock/arts/expressionism/scientist.jpg'
+
+// Conceptual Art
+import DateWithArt from 'src/assets/images/mock/arts/conceptual_art/a_date_with_art.jpg'
+import Artist from 'src/assets/images/mock/arts/conceptual_art/artist.jpg'
+import ConceptualismInPhilosophy from 'src/assets/images/mock/arts/conceptual_art/conceptualism_in_philosophy.jpg'
+import GlamorInTheatricalScenery from 'src/assets/images/mock/arts/conceptual_art/glamor_in_theatrical_scenery.jpg'
+import LemonEaters from 'src/assets/images/mock/arts/conceptual_art/lemon_eaters.jpg'
+
+// Figurative Art
+import CubismStillLife from 'src/assets/images/mock/arts/figurative_art/cubism_still_life.jpg'
+import Cubism from 'src/assets/images/mock/arts/figurative_art/cubism.jpg'
+import Ontario from 'src/assets/images/mock/arts/figurative_art/ontario.jpg'
+
+// Interior Art
+import ClassicsInInterior from 'src/assets/images/mock/arts/interior_art/classics_in_a_modern_interior.jpg'
+import CozyCorner from 'src/assets/images/mock/arts/interior_art/cozy_corner.jpg'
+import Curtains from 'src/assets/images/mock/arts/interior_art/curtains.jpg'
+import Old from 'src/assets/images/mock/arts/interior_art/old.jpg'
+import Paintings from 'src/assets/images/mock/arts/interior_art/paintings.jpg'
+import SpaceLightAndSilence from 'src/assets/images/mock/arts/interior_art/space_light_and_silence.jpg'
+import VanguardInInterior from 'src/assets/images/mock/arts/interior_art/vanguard_landscapes_in_the_interior.jpg'
+import Walls from 'src/assets/images/mock/arts/interior_art/walls.jpg'
+import WorkExamples from 'src/assets/images/mock/arts/interior_art/work_examples.jpg'
+
+// Pop Art
+import Glory from 'src/assets/images/mock/arts/pop_art/glory.jpg'
+import Kiss from 'src/assets/images/mock/arts/pop_art/kiss.jpg'
+import Mustache from 'src/assets/images/mock/arts/pop_art/mustache.jpg'
+import Portrait from 'src/assets/images/mock/arts/pop_art/portrait.jpg'
+import Vasilisa from 'src/assets/images/mock/arts/pop_art/vasilisa.jpg'
+import Woman from 'src/assets/images/mock/arts/pop_art/woman.jpg'
+import Zebra from 'src/assets/images/mock/arts/pop_art/zebra.jpg'
+
+// Street Art
+import NewMural from 'src/assets/images/mock/arts/street_art/new_mural.jpg'
+import Sigh from 'src/assets/images/mock/arts/street_art/sigh.jpg'
+import FallOfPhilosopher from 'src/assets/images/mock/arts/street_art/the_fall_of_a_philosopher.jpg'
+import Wall from 'src/assets/images/mock/arts/street_art/wall.jpg'
+
+// Architecture
+import TwelveTouches from 'src/assets/images/mock/arts/architecture/12_touches.jpg'
+import AbandonedHouse from 'src/assets/images/mock/arts/architecture/abandoned_house.jpg'
+import ArchitecturalLandscape from 'src/assets/images/mock/arts/architecture/architectural_landscape.jpg'
+import Cathedral from 'src/assets/images/mock/arts/architecture/cathedral.jpg'
+import Cubofuturism from 'src/assets/images/mock/arts/architecture/cubofuturism.jpg'
+import Metropolitan from 'src/assets/images/mock/arts/architecture/metropolitan.jpg'
+import Rain from 'src/assets/images/mock/arts/architecture/rain.jpg'
+import StreetTurn from 'src/assets/images/mock/arts/architecture/street_turn.jpg'
+import TimesOfDay from 'src/assets/images/mock/arts/architecture/times_of_day.jpg'
+
+export {
+  Event1,
+  Event2,
+  Event3,
+  Event4,
+  Event5,
+  Event6,
+  Event7,
+  Event8,
+  Crowd,
+  Eye,
+  Jellyfish,
+  Music,
+  Shine,
+  Smile,
+  Space,
+  Uturn,
+  EasyConversation,
+  Fatigue,
+  MountainsAndRiver,
+  Road,
+  Dance,
+  Farmer,
+  LastSupper,
+  Palette,
+  Robbers,
+  Scientist,
+  DateWithArt,
+  Artist,
+  ConceptualismInPhilosophy,
+  GlamorInTheatricalScenery,
+  LemonEaters,
+  CubismStillLife,
+  Cubism,
+  Ontario,
+  ClassicsInInterior,
+  CozyCorner,
+  Curtains,
+  Old,
+  Paintings,
+  SpaceLightAndSilence,
+  VanguardInInterior,
+  Walls,
+  WorkExamples,
+  Glory,
+  Kiss,
+  Mustache,
+  Portrait,
+  Vasilisa,
+  Woman,
+  Zebra,
+  NewMural,
+  Sigh,
+  FallOfPhilosopher,
+  Wall,
+  TwelveTouches,
+  AbandonedHouse,
+  ArchitecturalLandscape,
+  Cathedral,
+  Cubofuturism,
+  Metropolitan,
+  Rain,
+  StreetTurn,
+  TimesOfDay,
+}
